bf7e3aae5ed5bca2c81443714f7b93fe984ded9a
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-05-30 15:17:45 -0500

    Database created, edited and query resolved

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon%';
+SELECT name FROM animals WHERE date_of_birth >= '01/01/2016' AND date_of_birth <= '01/01/2019';
+SELECT name FROM animals WHERE escape_attempts < 3 AND neutered = true;
+SELECT date_of_birth FROM animals WHERE name = 'Pikachu' OR name = 'Agumon';
+SELECT name, escape_attempts FROM animals WHERE weight_kg >= 10.50;
+SELECT * FROM animals WHERE neutered = true; 
+SELECT * From animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
