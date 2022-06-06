f45640693cff966c5165b4222e4fc34c896e457c
Alexr16 <j.alex-1996@hotmail.com>
2022-06-06 16:02:22 -0500

    Add data into visits and owners table

--- data.sql
+++ data.sql
@@ -109,3 +109,10 @@ INSERT INTO visits VALUES ((SELECT id FROM animals WHERE name = 'Boarmon'), (SEL
 INSERT INTO visits VALUES ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2020-05-24');
 -- Blossom visited William Tatcher on Jan 11th, 2021.
 INSERT INTO visits VALUES ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2021-01-11');
+
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
