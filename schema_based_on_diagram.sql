01386852865219948bdd4e6aace1ba14291811ed
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-06-07 11:38:43 -0500

    invoice_items table created

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -11,3 +11,13 @@ CREATE TABLE patients (
   name VARCHAR(255),
   date_of_birth DATE,
 );
+
+CREATE TABLE invoice_items (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  unit_price DECIMAL(10,2),
+  quantity INTEGER,
+  total_price DECIMAL(10,2),
+  invoice_id INTEGER,
+  treatment_id INTEGER,
+  FOREIGN KEY (invoice_id) REFERENCES invoices (id)
+);
